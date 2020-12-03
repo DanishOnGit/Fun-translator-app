575d54fde93e95c2bce1b489991fd801cc0440e5
DanishOnGit <youcandoit1911@gmail.com>
2020-12-03 19:30:28 +0530

    feat: added event listeners

---
+++ app.js
@@ -0,0 +1,27 @@
+var inputText= document.querySelector("#input");
+var translateBtn=document.querySelector("#btn");
+var outputDiv=document.querySelector("#output");
+var serverUrl= "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslationUrl(text){
+    return serverUrl+"?"+"text="+text
+}
+
+function errorHandler(error){
+    alert ("Server down. Try after some time" + error);
+}
+
+
+
+function clickHandler(){
+   var userInput= inputText.value;
+    fetch(getTranslationUrl(userInput))
+    .then(response=>response.json())
+    .then(json=>outputDiv.innerText)
+    .catch(errorHandler)
+}
+
+
+
+
+translateBtn.addEventListener("click",clickHandler);
